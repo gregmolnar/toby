3cb92eead759db5dc1d36e402372b987dc5186d2
Greg Molnar <molnargerg@gmail.com>
2014-10-18 10:00:49 +0100

    fix fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,13 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = users(:one)
+    sign_in @user
+  end
+end
