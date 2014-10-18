d82181a2edb2cd83edbda842917285186e3115af
Greg Molnar <molnargerg@gmail.com>
2014-10-18 09:58:29 +0100

    add lockable

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 20141018081002) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
